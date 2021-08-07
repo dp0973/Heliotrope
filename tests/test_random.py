729ed06fcfd1d9308404419d473397dd12521b5e
dp0973 <yeong0973@gmail.com>
2021-08-07 23:20:34 +0900

    test(random): remove 404 test

--- tests/test_random.py
+++ tests/test_random.py
@@ -6,8 +6,3 @@ url = "/v5/api/hitomi/random"
 def test_random(app: TestManager):
     _, response = app.test_client.get(url)
     assert response.status == 200
-
-
-def test_random_not_found(app: TestManager):
-    _, response = app.test_client.get(url + "0")
-    assert response.status == 404
