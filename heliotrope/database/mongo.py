5287efb1328b51e1cd421c956e6ef58fca77e5fe
dp0973 <yeong0973@gmail.com>
2021-08-07 23:35:44 +0900

    fix(mongo): add to_list

--- heliotrope/database/mongo.py
+++ heliotrope/database/mongo.py
@@ -57,12 +57,12 @@ class NoSQLQuery:
     async def find_random_info(self) -> dict[str, Any]:
         return cast(
             dict[str, Any],
-            self.__collection.aggregate(
+            await self.__collection.aggregate(
                 [
                     {"$sample": {"size": 1}},
                     {"_id": 0},
                 ]
-            ),
+            ).to_list(1),
         )
 
     async def insert_info(self, info: dict[str, Any]) -> None:
