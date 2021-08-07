d8b75b28df1d31417a4f23809c32c9927668e41a
dp0973 <yeong0973@gmail.com>
2021-08-07 17:34:52 +0900

    test(tests/random): add random endpoint test

---
+++ tests/test_random.py
@@ -0,0 +1,8 @@
+from sanic_testing.manager import TestManager  # type:ignore
+
+url = "/v5/api/hitomi/random/"
+
+
+def test_random(app: TestManager):
+    _, response = app.test_client.get()
+    assert response.status == 200
