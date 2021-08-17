5b4ffd20a21c22250a558cd62ade15858502f670
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-17 15:11:15 +0900

    chore(github-actions): add docker ci/cd pipeline and workflow Integration (#33)
    * refactor(__init__): add detailed version info
    
    * chore(workflows): workflow Integration
    
    chore(workdlows): add docker ci/cd
    
    * chore(dockerfile): edit requirements path
    
    * chore(ci): add mongodb health check

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -11,4 +11,5 @@ class VersionInfo(NamedTuple):
 
 version_info = VersionInfo(major=5, minor=0, micro=0, releaselevel="alpha", serial=0)
 
-__version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}-{version_info.releaselevel}"
+__version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+__detailed_version__ = f"{__version__}-{version_info.releaselevel}"
